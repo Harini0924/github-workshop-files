d7cba52fb017f631366836fbb105b3e5cc80e390
Dan Pham <danpham0312@gmail.com>
2023-11-27 18:07:27 -0800

    message

--- random_number.py
+++ random_number.py
@@ -2,3 +2,9 @@ import random
 
 # random.randint(a,b) generates a random number between a and b inclusive.
 print(f"The random number is {random.randint(0,9)}")
+
+# adding comments
+# gdsc workshop
+print("I'm trying to create a merge conflict")
+
+
